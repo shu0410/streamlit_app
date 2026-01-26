6d56ae531fd3f5e323d9c1648f0e2e4fd8b57891
shu0410 <t125046@ed.sus.ac.jp>
2026-01-26 16:57:55 +0900

    サイドバーを追加

--- app.py
+++ app.py
@@ -3,25 +3,27 @@ import pandas as pd
 
 st.title("年齢別・男女別人口アプリ")
 
-df = pd.read_csv("population.csv")
-st.write(df.head())
+with st.sidebar
 
-sex = st.radio(
-    "表示する性別を選択",
-    ["男性", "女性", "両方"]
-)
+    df = pd.read_csv("population.csv")
+    st.write(df.head())
 
-age_min, age_max = st.slider(
-    "年齢範囲を選択",
-    min_value=0,
-    max_value=100,
-    value=(0, 100)
-)
+    sex = st.radio(
+        "表示する性別を選択",
+        ["男性", "女性", "両方"]
+    )
 
-filtered = df[
-    (df["age-start"] >= age_min) &
-    (df["age-start"] <= age_max)
-]
+    age_min, age_max = st.slider(
+        "年齢範囲を選択",
+        min_value=0,
+        max_value=100,
+        value=(0, 100)
+    )
+
+    filtered = df[
+        (df["age-start"] >= age_min) &
+        (df["age-start"] <= age_max)
+    ]
 
 if sex == "男性":
     show_df = filtered.set_index("Age Group (5-year intervals)")[["Male Population"]]
