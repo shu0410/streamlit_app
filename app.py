3569e63253b264a7dcdce945e199d37efbcdc19b
shu0410 <t125046@ed.sus.ac.jp>
2026-01-30 23:04:29 +0900

    テキストのダウンロードの修正３

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ st.warning("⚠ Needs review")
 
 st.text("Read Me!!")
 st.download_button(
-    label="テキストをダウンロード":
+    label="テキストをダウンロード",
     data="閲覧してくださりありがとうございます。",
     file_name="Thank you.txt"
 )
